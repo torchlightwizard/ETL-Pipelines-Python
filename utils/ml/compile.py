3773ade42c4ed29e2385b08bbb8d8120e2f3c9e2
Torch <rustruedetective@gmail.com>
2025-04-13 06:30:20 +0500

    added compile_and_fit method, for lstm, for stock data

---
+++ utils/ml/compile.py
@@ -0,0 +1,10 @@
+from utils.ml.globals import MAX_EPOCHS
+import tensorflow as tf
+
+
+
+def compile_and_fit(model, window, patience=2):
+  early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=patience, mode='min')
+  model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=tf.keras.optimizers.Adam(), metrics=[tf.keras.metrics.MeanAbsoluteError()])
+  history = model.fit(window.train, epochs=MAX_EPOCHS, validation_data=window.val, callbacks=[early_stopping])
+  return history
