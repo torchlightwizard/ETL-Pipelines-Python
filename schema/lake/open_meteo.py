659d155898c8a4b9d260d7a78738621395d99425
Torch <rustruedetective@gmail.com>
2025-04-16 08:23:17 +0500

    added open meteo lake schema

---
+++ schema/lake/open_meteo.py
@@ -0,0 +1,9 @@
+schema = [
+    {
+        "table_name": "open_meteo",
+        "columns": {
+            "id": "INT NOT NULL AUTO_INCREMENT PRIMARY KEY",
+            "data": "JSON NULL"
+        }
+    }
+]
