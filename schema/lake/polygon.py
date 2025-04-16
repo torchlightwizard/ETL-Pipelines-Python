f19970b0f820835a36dee7699859ecdaa755607c
Torch <rustruedetective@gmail.com>
2025-04-16 08:23:48 +0500

    added polygon lake schema

---
+++ schema/lake/polygon.py
@@ -0,0 +1,9 @@
+schema = [
+    {
+        "table_name": "polygon",
+        "columns": {
+            "id": "INT NOT NULL AUTO_INCREMENT PRIMARY KEY",
+            "data": "JSON NULL"
+        }
+    }
+]
